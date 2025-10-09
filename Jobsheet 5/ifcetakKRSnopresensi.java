dcc2e15552f89bb7d939d3ead76457a220dea105
iGipxy <gesangrah@gmail.com>
2025-10-09 07:23:05 +0700

    Percobaan 1

--- Jobsheet 5/ifcetakKRSnopresensi.java
+++ Jobsheet 5/ifcetakKRSnopresensi.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class ifcetakKRSnopresensi {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
         System.out.println("---Cetak KRS SIAKAD---");
         System.out.print("apakah UKT sudah lunas? (True/False): ");
-            if(uktLunas) {
-                System.out.println("Pembayaran UKT Terferifikasi");
-                System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-            }
- 
+        boolean uktLunas = input.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT Terferifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        }
     }
 }
