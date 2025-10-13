59b812730315b127e9701f8247993da4515f08b2
iGipxy <gesangrah@gmail.com>
2025-10-13 19:19:12 +0700

    Update test.java

--- test.java
+++ test.java
@@ -3,22 +3,57 @@ import java.util.Scanner;
 public class test {
 
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
+        Scanner sc = new Scanner(System.in);
 
-            System.out.print("input kelas lur:  ");
-            String kelas = sc.nextLine();
-            System.out.println("input sub kelas: ");
-            String subkelas = sc.nextLine();
-            
-            if(kelas.equals("kelas 1")){
-                switch (subkelas) {
-                    case "a" -> System.out.println();
-                    case "b" -> System.out.println();
-                    default -> System.out.println();
-                }
-            }else{
-                System.out.println("nuh uh");
-            }
+        String employeeName = "null";
+
+        Double midYearEvalTechnical, endYearEvalTechnical, workProjectTechnical, finalScoreTechnical ,avgTechnical;
+        Double midYearEvalDiscipline, endYearEvalDiscipline, workProjectDiscipline, finalScoreDiscpline, avgDiscipline;
+
+        System.out.print("Enter Employee Name: ");
+        employeeName = sc.nextLine();
+
+        System.out.println("TECHNICAL SKILLS EVALUATION");
+        System.out.println("Enter the score for the following parameters (0-100)");
+        System.out.print("Technical skilss - Mid Year Evaluation: ");
+        midYearEvalTechnical = sc.nextDouble();
+        System.out.print("Technical skilss - end Year Evaluation: ");
+        endYearEvalTechnical = sc.nextDouble();
+        System.out.print("Technical skilss -  Work project: ");
+        workProjectTechnical = sc.nextDouble();
+        
+        finalScoreTechnical = (midYearEvalTechnical * 0.3) + (endYearEvalTechnical * 0.4) + (workProjectTechnical * 0.3);
+        
+        System.out.println("WORK DISCIPLINE EVALUATION");
+        System.out.print("Work discipline - Mid Year Evaluation: ");
+        midYearEvalDiscipline = sc.nextDouble();
+        System.out.print("Work discpiline - end Year Evaluation: ");
+        endYearEvalDiscipline = sc.nextDouble();
+        System.out.print("Work discipline -  Work project: ");
+        workProjectDiscipline = sc.nextDouble();
+        
+        finalScoreDiscpline = (midYearEvalDiscipline * 0.3) + (endYearEvalDiscipline * 0.4) + (workProjectDiscipline * 0.3);
+
+        if(finalScoreTechnical >= 60){
+            System.out.println(employeeName + " Passed the technical skills evaluation with a score of " + finalScoreTechnical);
+
+        }else{
+            System.out.println(employeeName + " Did not pass the technical skills evaluation with a score of " + finalScoreTechnical);
         }
+
+        if(finalScoreDiscpline >= 60){
+            System.out.println(employeeName + " Passed the technical skills evaluation with a score of " + finalScoreDiscpline);
+
+        }else{
+            System.out.println(employeeName + " Did not pass the technical skills evaluation with a score of " + finalScoreDiscpline);
+        }
+
+        //Promotion criteria
+        if(finalScoreTechnical >= 70 && finalScoreDiscpline >= 70){
+            System.out.println(employeeName + " is eligible for promotion");
+        }else{
+            System.out.println(employeeName + " is not eligible for promotion");
+        }
+        sc.close();
     }
 }
