835af3eb07e562ada035c809d3fa81dc2fcab018
iGipxy <gesangrah@gmail.com>
2025-10-13 09:19:05 +0700

    add new jobsheet

---
+++ Jobsheet 6/caseStudy.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+Public Class caseStudy {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int utsAlgo, uasAlgo, tugasAlgo;
+        int utStruktur, uasStruktur, tugasStruktur;
+    }
+}
