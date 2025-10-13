02dd5637c46b83c21e1d4dcc26eb34beb6366277
iGipxy <gesangrah@gmail.com>
2025-10-13 09:46:20 +0700

    fix RAHH

--- test.java
+++ test.java
@@ -3,27 +3,22 @@ import java.util.Scanner;
 public class test {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
 
-        System.out.print("input kelas lur:  ");
-        String kelas = sc.nextLine();
-        System.out.println("input sub kelas: ");
-        String subkelas = sc.nextLine();
-        
-        if(kelas == "kelas 1"){
-            if(subkelas == "a"){
-                System.out.println();
-            } else if (subkelas == "b") {
-                System.out.println();
+            System.out.print("input kelas lur:  ");
+            String kelas = sc.nextLine();
+            System.out.println("input sub kelas: ");
+            String subkelas = sc.nextLine();
+            
+            if(kelas.equals("kelas 1")){
+                switch (subkelas) {
+                    case "a" -> System.out.println();
+                    case "b" -> System.out.println();
+                    default -> System.out.println();
+                }
             }else{
-                System.out.println();
+                System.out.println("nuh uh");
             }
-        }else{
-            System.out.println("nuh uh");
         }
-        
-
-
-        sc.close();
     }
 }
